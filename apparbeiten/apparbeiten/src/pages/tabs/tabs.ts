68e667fc8e4d773555c1c5623e5d69eca2b62390
jHilscher <joerg.hilscher@gmail.com>
2016-12-07 09:58:49 +0100

    merge

--- apparbeiten/apparbeiten/src/pages/tabs/tabs.ts
+++ apparbeiten/apparbeiten/src/pages/tabs/tabs.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
-import { AboutPage } from '../vorhaben/vorhaben';
-import { ContactPage } from '../aufgaben/aufgaben';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   templateUrl: 'tabs.html'
