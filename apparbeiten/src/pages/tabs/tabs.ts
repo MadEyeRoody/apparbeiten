1d9bb6879b37b2e40f9874e7eac4f09ce219a6d0
Roody <rudi.mottinger@gmail.com>
2016-12-07 09:51:11 +0100

    Renamed about -> Vorhaben

--- apparbeiten/src/pages/tabs/tabs.ts
+++ apparbeiten/src/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
+import { AboutPage } from '../vorhaben/about';
 import { ContactPage } from '../aufgaben/aufgaben';
 
 @Component({
